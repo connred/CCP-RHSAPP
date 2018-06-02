8ee934d01310ac91e16b0477d56fc54fcf224a72
Connor Redmond <connred@icloud.com>
2018-06-02 11:12:34 -0500

    First commit

---
+++ RHS-APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RHS-APP
+//
+//  Created by Connor Redmond on 6/1/18.
+//  Copyright © 2018 RHS CCPGlobal Interns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RHS-APPTests/RHS_APPTests.swift
@@ -0,0 +1,36 @@
+//
+//  RHS_APPTests.swift
+//  RHS-APPTests
+//
+//  Created by Connor Redmond on 6/1/18.
+//  Copyright © 2018 RHS CCPGlobal Interns. All rights reserved.
+//
+
+import XCTest
+@testable import RHS_APP
+
+class RHS_APPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
